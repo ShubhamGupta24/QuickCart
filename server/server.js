a233cf26ab5767f81ec27b41facffb2c74246d9c
ShubhamGupta24 <guptashubhamofficial24@gmail.com>
2024-06-17 22:41:50 +0530

    QuickCart: Updated

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ const PORT = process.env.PORT || 5000;
 
 
 connectDb().then(() => {
-  app.listen(PORT,'0.0.0.0', () => {
+  app.listen(PORT,() => {
     console.log(`server is running at port: ${PORT}`);
   });
 });
